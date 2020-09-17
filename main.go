dd4374eb54587e8c33cce397404aa5b693d1fc88
Josh McSavaney <me@mcsau.cc>
2020-09-17 13:28:55 -0400

    move stream json fetcher to own function

--- main.go
+++ main.go
@@ -7,14 +7,27 @@ import (
 	"net/http"
 )
 
-func main() {
-	resp, err := http.Get("https://builds.coreos.fedoraproject.org/streams/stable.json")
-	if err != nil || resp.StatusCode != http.StatusOK {
-		log.Fatalf("failed to fetch streams json: %w", resp)
+func streamjson(stream string) ([]byte, error) {
+	url := "https://builds.coreos.fedoraproject.org/streams/" + stream + ".json"
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch %v: %w", url, err)
 	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("got non-OK status when fetching %v: %v", url, resp.Status)
+	}
+
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read body of %v: %w", url, err)
+	}
+	return bodyBytes, nil
+}
+
+func main() {
+	j, err := streamjson("stable")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(bodyBytes))
+	fmt.Println(string(j))
 }
